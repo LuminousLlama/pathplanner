78d1e37077419a2e76c93e6e4bbbf737dadbe89d
LuminousLlama <keyushattarde@gmail.com>
2023-02-18 00:56:56 -0800

    flip on x direction

--- pathplannerlib/src/main/java/com/pathplanner/lib/PathPlannerTrajectory.java
+++ pathplannerlib/src/main/java/com/pathplanner/lib/PathPlannerTrajectory.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 public class PathPlannerTrajectory extends Trajectory {
   private static final double FIELD_WIDTH_METERS = 8.02;
+  //FIXME: add the correct number 
+  private static final double FIELD_LENGTH_METERS = 16.02;
 
   private final List<EventMarker> markers;
   private final StopEvent startStopEvent;
@@ -121,9 +123,9 @@ public class PathPlannerTrajectory extends Trajectory {
       PathPlannerState transformedState = new PathPlannerState();
 
       Translation2d transformedTranslation =
-          new Translation2d(state.poseMeters.getX(), FIELD_WIDTH_METERS - state.poseMeters.getY());
+          new Translation2d(FIELD_LENGTH_METERS - state.poseMeters.getX(), state.poseMeters.getY());
       Rotation2d transformedHeading = state.poseMeters.getRotation().times(-1);
-      Rotation2d transformedHolonomicRotation = state.holonomicRotation.times(-1);
+      Rotation2d transformedHolonomicRotation = state.holonomicRotation.rotateBy(Rotation2d.fromDegrees(180));
 
       transformedState.timeSeconds = state.timeSeconds;
       transformedState.velocityMetersPerSecond = state.velocityMetersPerSecond;
